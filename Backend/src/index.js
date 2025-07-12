07434ccc0d398a9536520cd64462f3c3e5e0c895
Pritam Chakraborty <123851661+rahulisbusy@users.noreply.github.com>
2025-07-12 10:48:03 +0530

    schema updated

--- Backend/src/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import { app } from "./app.js";
-
-
-
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`✅ Server is running at port: ${process.env.PORT}`);
-});
